de7f8aedeb69fe849d2be1641c92dd15bac92ea4
Md Tuser Sheikh <tuser.sheikh@zoho.com>
2020-04-17 21:01:51 +0600

    add Calculator component

---
+++ src/components/Calulator.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Calculator extends Component {
+    render() {
+        return <h2>This is calculator</h2>;
+    }
+}
+
+export default Calculator;
