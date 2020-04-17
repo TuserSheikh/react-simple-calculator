8b87f9044fc53e1107500dee801c5d073346df69
Razu Ahmed <razu.exe@gmail.com>
2020-04-17 21:46:43 +0600

    file ready for work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Calculator from "./components/Calulator";
-import "./App.css";
 
 class App extends Component {
     render() {
--- src/components/Calulator.jsx
+++ src/components/Calulator.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-
+// import { Container, Row, Col, Button } from 'react-bootstrap';
+import './calculator.css'
 class Calculator extends Component {
     render() {
-        return <h2>This is calculator</h2>;
+        return(
+        <div><h2>This is calculator</h2></div>
+        )
     }
 }
 
